cf5942fac5f487add8c9bb1812a981ee873f3289
Sandor Kis <kissandor@hotmail.com>
2018-01-26 21:19:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
-var funkyFunction = function() {
+function funkyFunction() {
   return function() {
     return "FUNKY!"
   }
